c7744a521dcb2f8cae5f2fd13fc023e39dd60cc9
Christian Gaetano <hello@christiangaetano.com>
2025-01-26 17:17:57 -0800

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+  integrations: [tailwind()],
 });
